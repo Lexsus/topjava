09b6642f57162953fc668ffe80f7a27a9d322cfa
Lexsus <lucassrus@gmail.com>
2022-10-14 09:43:03 +0300

    HW02.  -fix logback version  -add User methods

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,15 +3,28 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.UserUtils;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        UserUtils.users.forEach(this::save);
+    }
 
     @Override
     public boolean delete(int id) {
@@ -22,7 +35,13 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        // handle case: update, but not present in storage
+        return repository.computeIfPresent(user.getId(), (id, oldMeal) -> user);
     }
 
     @Override
---
+++ src/main/java/ru/javawebinar/topjava/util/UserUtils.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+public class UserUtils {
+    public static final List<User> users  = Arrays.asList(
+            new User(0, "Александр Иванов", "AIvanov@gmail.com", "111", Role.ADMIN),
+            new User(1, "Сидоров Петр", "PSidorov@gmail.com", "222", Role.USER),
+            new User(2, "Кузнецов Андрей", "AKuznezov@gmail.com", "333", Role.USER)
+    );
+}
