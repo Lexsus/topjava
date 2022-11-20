6aa226f05a92a01016945d658a7883766bf9f4af
Lexsus <lucassrus@gmail.com>
2022-11-20 21:31:31 +0300

    add roles to JdbcUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,17 +1,25 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcUserRepository implements UserRepository {
@@ -71,6 +79,46 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return jdbcTemplate.query("SELECT * FROM users  u LEFT JOIN user_roles d ON u.id = d.user_id ORDER BY name, email", new UsersWithRolesExtractor());
+//        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+    }
+
+    private static final class UsersWithRolesExtractor implements ResultSetExtractor<List<User>> {
+
+        @Override
+        public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            Map<Integer, User> map = new HashMap<>();
+            User user;
+            while (rs.next()) {
+                Integer id = rs.getInt("id");
+                user = map.get(id);
+                if (user == null) {
+                    user = new User();
+                    user.setId(id);
+                    user.setName(rs.getString("name"));
+                    user.setEmail(rs.getString("email"));
+                    user.setEnabled(rs.getBoolean("enabled"));
+                    user.setPassword(rs.getString("password"));
+                    user.setRegistered(rs.getDate("registered"));
+                    user.setCaloriesPerDay(rs.getInt("calories_per_day"));
+                    user.setRoles(new ArrayList<>());
+                    map.put(id, user);
+
+                }
+                String roleName = rs.getString("role");
+                if (roleName != null) {
+                    Role role = null;
+                    if (Role.USER.name().equals(roleName)) {
+                        role = Role.USER;
+                    } else if (Role.ADMIN.name().equals(roleName)) {
+                        role = Role.ADMIN;
+                    }
+                    if (role != null) {
+                        user.getRoles().add(role);
+                    }
+                }
+            }
+            return new ArrayList<>(map.values());
+        }
     }
 }
